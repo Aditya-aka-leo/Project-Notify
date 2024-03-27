e0b6ab922f356f05ed57df489d800dff60789210
Aditya Agarwal <leohaxter@gmail.com>
2024-03-27 23:28:15 +0530

    Added the support to notify the users

--- server/src/routes/main.js
+++ server/src/routes/main.js
@@ -1,6 +1,8 @@
 const express = require("express");
-const {create_user} = require('../controller/controller'); 
+const {create_user,notify_user} = require('../controller/controller'); 
 const router = express.Router();
 
 router.post("/create_user", create_user);
+router.post('/notify',notify_user);
 module.exports = router;
+
