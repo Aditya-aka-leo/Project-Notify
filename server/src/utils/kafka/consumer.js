7bf5dd9177980b39f4a1c71246fa4a0a549a0561
haddo <hjain.1523@gmail.com>
2024-03-28 19:41:36 +0530

    Created consumers for partitions and consumer-groups

--- server/src/utils/kafka/consumer.js
+++ server/src/utils/kafka/consumer.js
@@ -45,16 +45,29 @@ const run = async () => {
     groupId: "Push Notificaiton",
   });
   const Validation_consumer = kafka.consumer({ groupId: "Validation" });
-  const High_Priority_consumer = kafka.consumer({ groupId: "High-p" });
-  const Mid_Priority_consumer = kafka.consumer({ groupId: "Low-p" });
+  const High_Priority_consumer1 = kafka.consumer({ groupId: "High-p" });
+  const High_Priority_consumer2 = kafka.consumer({ groupId: "High-p" });
+  const High_Priority_consumer3 = kafka.consumer({ groupId: "High-p" });
+  const High_Priority_consumer4 = kafka.consumer({ groupId: "High-p" });
+  const High_Priority_consumer5 = kafka.consumer({ groupId: "High-p" });
+
+  const Mid_Priority_consumer1 = kafka.consumer({ groupId: "Low-p" });
+  const Mid_Priority_consumer2 = kafka.consumer({ groupId: "Low-p" });
+  const Mid_Priority_consumer3 = kafka.consumer({ groupId: "Low-p" });
   const Low_Priority_consumer = kafka.consumer({ groupId: "Mid-p" });
 
   await consume("Push-Notification", Push_Notification_consumer);
   await consume("Ivr", Ivr_consumer);
   await consume("Sms", sms_consumer);
   await consume("Email", email_consumer);
-  await consume("High-priority", High_Priority_consumer);
-  await consume("Mid-priority", Mid_Priority_consumer);
+  await consume("High-priority", High_Priority_consumer1);
+  await consume("High-priority", High_Priority_consumer2);
+  await consume("High-priority", High_Priority_consumer3);
+  await consume("High-priority", High_Priority_consumer4);
+  await consume("High-priority", High_Priority_consumer5);
+  await consume("Mid-priority", Mid_Priority_consumer1);
+  await consume("Mid-priority", Mid_Priority_consumer2);
+  await consume("Mid-priority", Mid_Priority_consumer3);
   await consume("Low-priority", Low_Priority_consumer);
   await consume("Validation", Validation_consumer);
   run_consumer(Push_Notification_consumer, send_push_notification);
@@ -62,9 +75,14 @@ const run = async () => {
   run_consumer(sms_consumer, send_sms);
   run_consumer(email_consumer, send_email);
   run_consumer(Validation_consumer, Validator_Prioritizer_Prod);
-  run_consumer(High_Priority_consumer, Service_Selector_Prod);
-  run_consumer(Mid_Priority_consumer, Service_Selector_Prod);
+  run_consumer(High_Priority_consumer1, Service_Selector_Prod);
+  run_consumer(High_Priority_consumer2, Service_Selector_Prod);
+  run_consumer(High_Priority_consumer3, Service_Selector_Prod);
+  run_consumer(High_Priority_consumer4, Service_Selector_Prod);
+  run_consumer(High_Priority_consumer5, Service_Selector_Prod);
+  run_consumer(Mid_Priority_consumer1, Service_Selector_Prod);
+  run_consumer(Mid_Priority_consumer2, Service_Selector_Prod);
+  run_consumer(Mid_Priority_consumer3, Service_Selector_Prod);
   run_consumer(Low_Priority_consumer, Service_Selector_Prod_Bulk);
 };
 module.exports = { run };
-
